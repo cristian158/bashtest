5d09e20b7fdcecab96a5261d194fefe675bcb55e
cristian <cnovoa.o@gmail.com>
2021-07-19 19:09:53 -0400

    b

--- samplescript1.sh
+++ samplescript1.sh
@@ -1,12 +1,17 @@
+#doar manjar
 sudo pacman-mirrors --fasttrack
+
+#general
 sudo pacman -Syu
 sudo pacman -S base-devel
 
+#yay
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 sudo pacman -U yay-10.3.0-1-x86_64.pkg.tar.zst
 
+#softwari
 sudo pacman -S ardour audacious clamtk
 sudo pacman -S atom
 sudo pacman -S audacity firefox kdenlive
@@ -14,14 +19,14 @@ sudo pacman -S mgba-qt musescore nicotine+
 sudo pacman -S qbittorrent redshift spotifyd virtualbox vlc
 yay -S auto-cpufreq bitwarden fastfetch stacer timeshift
 
-#themes
+#themes icons
 sudo pacman -S gtk-engine-murrine gtk-engines
 
 git clone https://github.com/vinceliuice/Matcha-gtk-theme
 cd Matcha-gtk-theme
 ./install.sh -c dark -t sea
-cd..
+cd ..
 
-git clone https://github.com/vinceliuice/Qogir-icon-theme 
+git clone https://github.com/vinceliuice/Qogir-icon-theme
 cd Qogir-icon-theme
-./ install .sh
+./install.sh
