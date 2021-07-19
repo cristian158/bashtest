b4987cbed20014ee92ab53a2d6ed6b3a9a3a105a
cristian <cnovoa.o@gmail.com>
2021-07-19 15:04:48 -0400

    a

--- samplescript1.sh
+++ samplescript1.sh
@@ -1,4 +1,9 @@
 sudo pacman-mirrors --fasttrack
 sudo pacman -Syu
 sudo pacman -S base-devel
-sudo pacman -S auto-cpufreq ardour atom audacious audacity bitwarden clamtk fastfetch firefox kdenlive mgba-qt musescore nicotine+ qbittorrent redshift spotifyd stacer timeshift virtualbox vlc
+sudo pacman -S ardour audacious clamtk
+sudo pacman -S atom
+sudo pacman -S audacity firefox kdenlive
+sudo pacman -S mgba-qt musescore nicotine+
+sudo pacman -S qbittorrent redshift spotifyd virtualbox vlc yay
+yay auto-cpufreq bitwarden fastfetch stacer timeshift
