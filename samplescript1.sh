5d880793740e81df9f8b044323dc415c36daa737
cristian <cnovoa.o@gmail.com>
2021-07-20 11:00:03 -0400

    Instructions

--- samplescript1.sh
+++ samplescript1.sh
@@ -1,62 +1,208 @@
-#CASE STATEMENT
 #doar manjar
 
-read -p "Want pacman-mirrors Y/N " ANSWER
+read -p "Want pacman-mirrors? Y/N " ANSWER
 case "$ANSWER" in
  [yY] | [yY][eE][sS])
    sudo pacman-mirrors --fasttrack
    ;;
  [nN] | [nN][oO])
-   echo '====================================='
+   echo '=================SKIPPING===================='
    ;;
  *)
    echo "Please enter y/yes or n/no"
    ;;
 esac
-# sudo pacman-mirrors --fasttrack
+
 
 #general
-sudo pacman -Syu
-echo "FULL UPDATE DONE"
-echo '====================================='
-sudo pacman -S base-devel
-echo "base-devel DONE"
-echo '====================================='
+read -p "Want Full Update? Y/N " ANSWER
+case "$ANSWER" in
+ [yY] | [yY][eE][sS])
+   sudo pacman -Syu
+   echo "FULL UPDATE DONE"
+   echo '====================================='
+   ;;
+ [nN] | [nN][oO])
+   echo '=================SKIPPING===================='
+   ;;
+ *)
+   echo "Please enter y/yes or n/no"
+   ;;
+esac
+
+read -p "Want base-devel? Y/N " ANSWER
+case "$ANSWER" in
+ [yY] | [yY][eE][sS])
+   sudo pacman -S base-devel
+   echo "base-devel DONE"
+   echo '====================================='
+   ;;
+ [nN] | [nN][oO])
+   echo '=================SKIPPING===================='
+   ;;
+ *)
+   echo "Please enter y/yes or n/no"
+   ;;
+esac
+
 
 #yay
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-sudo pacman -U yay-10.3.0-1-x86_64.pkg.tar.zst
-echo "YAY INSTALL DONE"
-echo '====================================='
+read -p "Want yay? Y/N " ANSWER
+case "$ANSWER" in
+ [yY] | [yY][eE][sS])
+   git clone https://aur.archlinux.org/yay.git
+   cd yay
+   makepkg -si
+   sudo pacman -U yay-10.3.0-1-x86_64.pkg.tar.zst
+   echo "YAY INSTALL DONE"
+   echo '====================================='
+   ;;
+ [nN] | [nN][oO])
+   echo '=================SKIPPING===================='
+   ;;
+ *)
+   echo "Please enter y/yes or n/no"
+   ;;
+esac
+
 
 #softwari
-sudo pacman -S ardour audacious clamtk
-echo "SOFTWARE 1/6 DONE"
-sudo pacman -S atom
-echo "SOFTWARE 2/6 DONE"
-sudo pacman -S audacity firefox kdenlive
-echo "SOFTWARE 3/6 DONE"
-sudo pacman -S mgba-qt musescore nicotine+
-echo "SOFTWARE 4/6 DONE"
-sudo pacman -S qbittorrent redshift spotifyd virtualbox vlc
-echo "SOFTWARE 5/6 DONE"
-yay -S auto-cpufreq bitwarden fastfetch stacer timeshift
-echo "SOFTWARE 6/6 DONE"
-echo '====================================='
+read -p "Want ardour/audacious/clamtk? Y/N" ANSWER
+case "$ANSWER" in
+  [yY] | [yY][eE][sS])
+    sudo pacman -S ardour audacious clamtk
+    echo "SOFTWARE 1/6 DONE"
+    echo '====================================='
+    ;;
+  [nN] | [nN][oO])
+   echo '=================SKIPPING===================='
+   ;;
+  *)
+   echo "Please enter Y/N"
+   ;;
+esac
+
+read -p "Want atom? (107M) Y/N" ANSWER
+case "$ANSWER" in
+  [yY] | [yY][eE][sS])
+    sudo pacman -S atom
+    echo "SOFTWARE 2/6 DONE"
+    echo '====================================='
+    ;;
+  [nN] | [nN][oO])
+    echo '=================SKIPPING===================='
+    ;;
+  *)
+    echo "Please enter Y/N"
+    ;;
+esac
+
+read -p "Want audacity/firefox/kdenlive? (216M) Y/N" ANSWER
+case "$ANSWER" in
+  [yY] | [yY][eE][sS])
+    sudo pacman -S audacity firefox kdenlive
+    echo "SOFTWARE 3/6 DONE"
+    echo '====================================='
+    ;;
+  [nN] | [nN][oO])
+    echo '=================SKIPPING===================='
+    ;;
+  *)
+    echo "Please enter Y/N"
+    ;;
+esac
+
+read -p "Want mgba/musescore/nicotine? (43M) Y/N" ANSWER
+case "$ANSWER" in
+  [yY] | [yY][eE][sS])
+    sudo pacman -S mgba-qt musescore nicotine+
+    echo "SOFTWARE 4/6 DONE"
+    echo '====================================='
+    ;;
+  [nN] | [nN][oO])
+    echo '=================SKIPPING===================='
+    ;;
+  *)
+    echo "Please enter Y/N"
+    ;;
+esac
+
+read -p "Want qbittorrent/redshift/spotifyd/virtualbox/vlc? (68M) Y/N" ANSWER
+case "$ANSWER" in
+  [yY] | [yY][eE][sS])
+    sudo pacman -S qbittorrent redshift spotifyd virtualbox vlc
+    echo "SOFTWARE 5/6 DONE"
+    echo '====================================='
+    ;;
+  [nN] | [nN][oO])
+    echo '=================SKIPPING===================='
+    ;;
+  *)
+    echo "Please enter Y/N"
+    ;;
+esac
+
+read -p "Want auto-cpufreq/bitwarden/fastfetch/stacer/timeshift? Y/N" ANSWER
+case "$ANSWER" in
+  [yY] | [yY][eE][sS])
+    yay -S auto-cpufreq bitwarden fastfetch stacer timeshift
+    echo "SOFTWARE 6/6 DONE"
+    echo '====================================='
+    ;;
+  [nN] | [nN][oO])
+    echo '=================SKIPPING===================='
+    ;;
+  *)
+    echo "Nigga please"
+    ;;
+esac
+
+read -p "Want gtk engines? (1M) Y/N" ANSWER
+case "$ANSWER" in
+  [yY] | [yY][eE][sS])
+    sudo pacman -S gtk-engine-murrine gtk-engines
+    echo "GTK ENGINES DONE"
+    echo '====================================='
+    ;;
+  [nN] | [nN][oO])
+    echo '=================SKIPPING===================='
+    ;;
+  *)
+    echo "Nigga Please"
+    ;;
+esac
 
 #themes icons
-sudo pacman -S gtk-engine-murrine gtk-engines
-echo "GTK ENGINES DONE"
 
-git clone https://github.com/vinceliuice/Matcha-gtk-theme
-cd Matcha-gtk-theme
-./install.sh -c dark -t sea
-cd ..
-echo "GTK THEME DONE"
+read -p "Want Matcha theme? (12M) Y/N" ANSWER
+case "$ANSWER" in
+  [yY] | [yY][eE][sS])
+    git clone https://github.com/vinceliuice/Matcha-gtk-theme
+    cd Matcha-gtk-theme
+    ./install.sh -c dark -t sea
+    cd ..
+    echo "GTK THEME DONE"
+    ;;
+  [nN] | [nN][oO])
+    echo '=================SKIPPING===================='
+    ;;
+  *)
+    echo "Nigga Please"
+    ;;
+esac
 
-git clone https://github.com/vinceliuice/Qogir-icon-theme
-cd Qogir-icon-theme
-./install.sh
-echo "ICONS DONE"
+read -p "Want Icons? (14M) Y/N" ANSWER
+case "$ANSWER" in
+  [yY] | [yY][eE][sS])
+    git clone https://github.com/vinceliuice/Qogir-icon-theme
+    cd Qogir-icon-theme
+    ./install.sh
+    echo "ICONS DONE"
+    ;;
+  [nN] | [nN][oO])
+    echo '=================SKIPPING===================='
+    ;;
+  *)
+    echo "Nigga Please"
+    ;;
+esac
